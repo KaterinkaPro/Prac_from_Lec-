875b71afab2400cc9bc2b985e9dd2b03caf8ad61
KaterinkaPro <pronina.nw@mail.ru>
2022-08-14 22:41:11 +0300

    add first practice

---
+++ Practice001/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Найти позицию в массиве, на которой находится заданное число (индекс)*/
+
+int[] array = { 1, 3, 5, 7, 8, 9, 5, 6 };
+
+int n = array.Length;
+int find  = 6;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    
+    index++;
+
+}
